fb31d497e639bd434776d3ae1f448e5bdbba23e6
Alexander Kuziulenko <fitbiestudio@gmail.com>
2023-12-15 15:53:54 +0700

    Solution 155 TODO

--- 155.cs
+++ 155.cs
@@ -1,77 +1,85 @@
-// MinStack obj = new MinStack();
-// obj.Push(2147483646);
-// obj.Push(2147483646);
-// obj.Push(2147483647);
-// obj.Push(2147483647);
-// obj.Push(-2147483648);
-// obj.Pop();
-// obj.Pop();
-// obj.Pop();
-// obj.Pop();
-// obj.GetMin();
+MinStack obj = new MinStack();
+obj.Push(2147483646);
+obj.Push(2147483646);
+obj.Push(2147483647);
+obj.Push(2147483647);
+obj.Push(-2147483648);
+obj.Pop();
+obj.Pop();
+obj.Pop();
+obj.Pop();
+obj.GetMin();
 
 
 
-// public class MinStack
-// {
-//     private int[] values = Array.Empty<int>();
-//     private int last = -1;
+public class MinStack
+{
+    private int[] values = Array.Empty<int>();
+    private int last = -1;
 
-//     private List<int> minValues = new();
-//     private int minValue = int.MaxValue;
+    private Stack<int> minValues = new();
+    private int minValue = 0;
+    private bool minInitialized = false;
 
 
-//     public MinStack(){}
+    public MinStack(){}
     
 
-//     public void Push(int val) 
-//     {
-//         if (last + 1 >= values.Length) 
-//         { 
-//             int[] temp = new int [values.Length+1];
-//             Array.Copy(values, temp, values.Length);
-//             values = temp;
-//         }
+    public void Push(int val) 
+    {
+        if (last + 1 >= values.Length) 
+        { 
+            int[] temp = new int [values.Length+1];
+            Array.Copy(values, temp, values.Length);
+            values = temp;
+        }
 
-//         values[++last] = val;
+        values[++last] = val;
 
-//         if (val <= minValue)
-//         {
-//             minValues.Add(val);
-//             minValue = val;
-//         }
-//     }
+        if (!minInitialized)
+        {
+            minValue = val;
+            minInitialized = true;
+            return;
+        }
+
+        if (val <= minValue)
+        {
+            minValues.Push(minValue);
+            minValue = val;
+        }
+    }
     
 
-//     public void Pop() 
-//     {
-//         if (values[last] == minValues[minValues.Count-1])
-//         {
-//             minValues.RemoveAt(minValues.Count-1);
-//             minValue = minValues[minValues.Count-1];
-//         }
+    public void Pop() 
+    {
+        if (values[last] == minValue)
+        {
+            if (minValues.Count == 0) { minInitialized = false; }
+            else { minValue = minValues.Pop(); }
+        }
 
-//         int[] temp = new int [values.Length-1];
-//         Array.Copy(values, temp, values.Length-1);
-//         values = temp;
-//         last--;
-//     }
+        int[] temp = new int [values.Length-1];
+        Array.Copy(values, temp, values.Length-1);
+        values = temp;
+        last--;
+    }
     
 
-//     public int Peek()
-//     {
-//         return values[last];
-//     }
+    public int Peek()
+    {
+        return values[last];
+    }
 
 
-//     public int Top() 
-//     {
-//         return values[last];
-//     }
+    public int Top() 
+    {
+        return values[last];
+    }
     
 
-//     public int GetMin() 
-//     {
-//         return minValue;
-//     }
-// }
+    public int GetMin() 
+    {
+        return minValue;
+    }
+}

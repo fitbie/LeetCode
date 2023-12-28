09ac815e0694c75d73913c7fdb2bfc248c9c7560
Alexander Kuziulenko <fitbiestudio@gmail.com>
2023-12-28 22:09:02 +0700

    Redo 84 Solution

--- 84.cs
+++ 84.cs
@@ -2,25 +2,51 @@ public class Solution
 {
     public int LargestRectangleArea(int[] heights)
     {
+        int[] results = new int[heights.Length];
         int result = 0;
+
+        Stack<int> nextSmaller = new();
+
+        for (int i = heights.Length - 1; i >= 0; i--)
+        {
+            while (nextSmaller.TryPeek(out int next))
+            {
+                if (heights[next] >= heights[i])
+                {
+                    nextSmaller.Pop();
+                }
+                else
+                {
+                    results[i] = heights[i] * (next - i);
+                    nextSmaller.Push(i);
+                    break;
+                }
+            }
+
+            if (nextSmaller.Count == 0) { results[i] = heights[i] * (heights.Length - i); nextSmaller.Push(i); }
+        }
+        
+        nextSmaller.Clear();
 
         for (int i = 0; i < heights.Length; i++)
         {
-            int tempResult = heights[i];
+            while (nextSmaller.TryPeek(out int prev))
+            {
+                if (heights[prev] >= heights[i])
+                {
+                    nextSmaller.Pop();
+                }
+                else
+                {
+                    results[i] += heights[i] * (i - prev - 1);
+                    nextSmaller.Push(i);
+                    break;
+                }
+            }
+
+            if (nextSmaller.Count == 0) { results[i] += heights[i] * i; nextSmaller.Push(i); }
             
-            for (int j = i + 1; j < heights.Length; j++)
-            {
-                if (heights[i] <= heights[j]) { tempResult += heights[i]; }
-                else break;
-            }
-
-            for (int k = i -1; k >= 0; k--)
-            {
-                if (heights[i] <= heights[k]) { tempResult += heights[i]; }
-                else break;
-            }
-
-            result = result < tempResult ? tempResult : result;
+            result = result < results[i] ? results[i] : result;
         }
 
         return result;

898b65cd42d59c2bbeedaee94b22f0ae84a928b6
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-01-03 22:38:29 +0700

    Redo 153 Solution

--- 153.cs
+++ 153.cs
@@ -1,22 +1,27 @@
 Solution solution = new();
-solution.FindMin(new int[] { 3,4,5,1,2 });
+solution.FindMin(new int[] { 2, 1 });
 
 
 public class Solution {
     public int FindMin(int[] nums)
     {
+        if (nums.Length == 0) { return nums[0]; }
+
         int last = nums.Length;
         int first = -1;
         int cycleLast = nums.Length - 1;
 
-        while (first < last - 1)
+        if (nums[0] > nums[^1])
         {
-            int middle = (last + first) / 2;
-            if (nums[middle] < nums[cycleLast]) { break; }
+            while (first < last - 1)
+            {
+                int middle = (last + first) / 2;
 
-            if (nums[middle] < nums[^1]) { last = middle; }
-            else if (nums[middle] > nums[^1]) { first = middle; }
-            cycleLast = middle;
+                if (nums[middle] < nums[^1]) { last = middle; }
+                else if (nums[middle] > nums[^1]) { first = middle; }
+                else if (nums[middle] == nums[^1]) { break; }
+                cycleLast = nums[cycleLast] > nums[middle] ? cycleLast : middle;
+            }
         }
         
         return nums[(cycleLast + 1) % nums.Length];

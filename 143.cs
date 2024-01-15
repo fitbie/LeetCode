0d3db32d0a5dc19f529de9e222b4b09cc2740341
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-01-15 21:23:35 +0700

    143 Solution update

--- 143.cs
+++ 143.cs
@@ -6,31 +6,30 @@ public class Solution
 {
     public void ReorderList(ListNode head)
     {
-        Stack<ListNode> stack = new();
-        Queue<ListNode> queue = new();
+        List<ListNode> nodes = new();
 
         while (head != null)
         {
-            stack.Push(head);
-            queue.Enqueue(head);
+            nodes.Add(head);
             head = head.next;
         }
         
-        head = queue.Dequeue();
+        head = nodes[0];
         var current = head;
-        int i = 1;
-        int limit = stack.Count;
-        while (i <= limit)
+        int left = 1; // Bc we assighn below.
+        int right = nodes.Count - 1;
+
+        while (left <= right)
         {
-            current.next = stack.Pop();
+            current.next = nodes[right];
+            right--;
             current = current.next;
-            i++;
 
-            if (i > limit) { break; }
+            if (left > right) { break; }
 
-            current.next = queue.Dequeue();
+            current.next = nodes[left];
+            left++;
             current = current.next;
-            i++;
         }
 
         current.next = null;
@@ -47,3 +46,40 @@ public class ListNode
         this.next = next;
     }
 }
+
+
+// public class Solution 
+// {
+//     public void ReorderList(ListNode head)
+//     {
+//         Stack<ListNode> stack = new();
+//         Queue<ListNode> queue = new();
+
+//         while (head != null)
+//         {
+//             stack.Push(head);
+//             queue.Enqueue(head);
+//             head = head.next;
+//         }
+        
+//         head = queue.Dequeue();
+//         var current = head;
+//         int i = 1;
+//         int limit = stack.Count;
+//         while (i <= limit)
+//         {
+//             current.next = stack.Pop();
+//             current = current.next;
+//             i++;
+
+//             if (i > limit) { break; }
+
+//             current.next = queue.Dequeue();
+//             current = current.next;
+//             i++;
+//         }
+
+//         current.next = null;
+
+//     }
+// }

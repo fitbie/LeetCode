d356dda62dc2b7b0864096e7731b816a12fb0a25
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-06-02 15:35:55 +0700

    344 Solution

---
+++ 344.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public void ReverseString(char[] s) 
+    {
+        int left = 0;    
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            (s[right], s[left]) = (s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+}

595e74a7767bb163076b973c98a7b2bd5a8bb686
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-07-07 18:15:18 +0700

    1518 Solution

---
+++ 1518.cs
@@ -0,0 +1,18 @@
+public class Solution 
+{
+    public int NumWaterBottles(int numBottles, int numExchange) 
+    {
+        int result = 0;
+        int emptyBottles = 0;
+        while (numBottles > 0)
+        {
+            result += numBottles;
+            emptyBottles += numBottles;
+
+            numBottles = emptyBottles / numExchange;
+            emptyBottles %= numExchange;
+        }
+
+        return result;
+    }
+}
